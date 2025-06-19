daeecad7df6d293d4baebe77e0045959e2d1755d
Eduardo de Castilhos Gimenis <eduardodecastilhosgimenis@Eduardos-MacBook-Air.local>
2025-06-19 13:25:07 -0700

    working on the meshlib problem. Created a script that works in the editor, it takes a directory and creates a meshlib based on it, instead of being limited to one scene. I still can't fix the player's non-existent collision with the walls. I am really not sure what's causing this problem, but it's the next one to tackle in my list.

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_start_button_pressed():
 	# This function changes the current scene to our main game scene.
-	get_tree().change_scene_to_file("res://scenes/main/TestingGrounds.tscn")
+	get_tree().change_scene_to_file("res://scenes/main/MainGame.tscn")
 
 func _on_quit_button_pressed():
 	# This function closes the game.
